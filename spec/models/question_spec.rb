4267efd8bcc80b54d90b66d1ecbd4659d654c715
Gabe Tiller <gabectiller@gmail.com>
2017-11-21 21:46:51 -0500

    modified questions rspec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:question) {Question.create!(title: "New Question Title", body: "New Question Body") }
-
-  describe "attributes" do
+  let(:question) {Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
     it "has title and body attributes" do
-      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body")  
+      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body")
+    end
+
+    it "responds to resolved" do
+      expect(question).to respond_to(:resolved)
     end
   end
-end
