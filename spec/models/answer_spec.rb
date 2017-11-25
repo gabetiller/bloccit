0fc0d084a3732886b7b79568c35ca4dc66db3f55
Gabe Tiller <gabectiller@gmail.com>
2017-11-24 23:32:41 -0500

    hello world

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:question) {Question.create!(title: "New Question Title", body: "New Question Body") }
-  let(:answer) {Answer.create!(body: 'Answer Body', question: question) }
+  let(:answer) { Answer.new(body: 'Answer Body') }
 
   describe "attributes" do
     it "has a body attribute" do
